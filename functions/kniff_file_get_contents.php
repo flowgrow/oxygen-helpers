8c297c1e797dd0de41c396cac940f7b2d5c77f95
Florian Groh <florian.groh@tuwien.ac.at>
2022-04-04 19:22:29 +0200

    add sanity checks

--- functions/kniff_file_get_contents.php
+++ functions/kniff_file_get_contents.php
@@ -1,31 +1,33 @@
 <?php
 
-function kniff_file_get_contents($url)
-{
-  try {
-    return file_get_contents($url);
-  } catch (\Exception $ex) {
-    if (strpos($url, '://') === false) {
-      $upload_dir = wp_get_upload_dir();
-      $url = str_replace($upload_dir['basedir'], $upload_dir['baseurl'], $url);
+if (!function_exists('kniff_file_get_contents')) {
+  function kniff_file_get_contents($url)
+  {
+    try {
+      return file_get_contents($url);
+    } catch (\Exception $ex) {
+      if (strpos($url, '://') === false) {
+        $upload_dir = wp_get_upload_dir();
+        $url = str_replace($upload_dir['basedir'], $upload_dir['baseurl'], $url);
+      }
+
+      $urlparts = parse_url($url);
+      $domain = $urlparts['host'];
+      $split = explode(".", $domain);
+      $tld = $split[count($split) - 1];
+
+      $c = curl_init();
+      curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($c, CURLOPT_URL, $url);
+      if ($tld == "local") {
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
+      }
+      $contents = curl_exec($c);
+      curl_close($c);
+
+      if ($contents) return $contents;
+      else return FALSE;
     }
-
-    $urlparts = parse_url($url);
-    $domain = $urlparts['host'];
-    $split = explode(".", $domain);
-    $tld = $split[count($split) - 1];
-
-    $c = curl_init();
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($c, CURLOPT_URL, $url);
-    if ($tld == "local") {
-      curl_setopt($c, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($c, CURLOPT_SSL_VERIFYHOST, false);
-    }
-    $contents = curl_exec($c);
-    curl_close($c);
-
-    if ($contents) return $contents;
-    else return FALSE;
   }
 }
